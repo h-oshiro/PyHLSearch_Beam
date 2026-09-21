c85472c490f592695f7cb9f1fec1b9e1d83efaf4
h-oshiro <h_ooshiro@sysmac.co.jp>
2026-09-22 07:10:16 +0900

    nums修正

--- HLSearch_Beam.py
+++ HLSearch_Beam.py
@@ -77,7 +77,16 @@ class SearchConfig:
     """
     primes: list[int] = field(default_factory=lambda: generate_primes(1579))
     nums: list[list[int]] = field(default_factory=lambda: [
-        [1], [1], [4], [4], [i for i in range(11)], [i for i in range(13)], [i for i in range(17)], [i for i in range(19)], [i for i in range(23)], [i for i in range(29)], # 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
+        [1], 
+        [i for i in range(3)], 
+        [i for i in range(5)], 
+        [i for i in range(7)], 
+        [i for i in range(11)], 
+        [i for i in range(13)], 
+        [i for i in range(17)], 
+        [i for i in range(19)], 
+        [i for i in range(23)], 
+        [i for i in range(29)], # 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
         31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 
         73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 
         127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 
